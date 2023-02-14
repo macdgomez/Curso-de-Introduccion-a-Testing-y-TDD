351d83fb934b72cf72d1c7a0f5660820b6bb5cad
Daniel S. Gómez <ds.gomezreverte@edu.gva.es>
2023-02-14 20:19:30 +0100

    Mejorando los test y aserciones.

--- intro_unit_testing/Library/src/main/java/org/cefire/library/Book.java
+++ intro_unit_testing/Library/src/main/java/org/cefire/library/Book.java
@@ -1,5 +1,7 @@
 package org.cefire.library;
 
+import java.util.Objects;
+
 public class Book {
     private final String ISBN;
     private final String title;
@@ -22,5 +24,25 @@ public class Book {
         return author;
     }
 
+    @Override
+    public String toString() {
+        return "Book{" +
+                "ISBN='" + ISBN + '\'' +
+                ", title='" + title + '\'' +
+                ", author='" + author + '\'' +
+                '}';
+    }
 
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Book book = (Book) o;
+        return ISBN.equals(book.ISBN) && title.equals(book.title) && author.equals(book.author);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(ISBN, title, author);
+    }
 }
--- intro_unit_testing/Library/src/test/java/org/cefire/library/BookCollectionTest.java
+++ intro_unit_testing/Library/src/test/java/org/cefire/library/BookCollectionTest.java
@@ -3,6 +3,7 @@ package org.cefire.library;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class BookCollectionTest {
@@ -67,5 +68,70 @@ public class BookCollectionTest {
         BookCollection books = new BookCollection(new Book[]{});
         Assertions.assertTrue(books.find(titleToLocate).isEmpty());
     }
+
+    /**
+     * Falla si no se implementa el método equals en la clase Book
+     * El método toString de la lase Book ayuda a identificar un objeto a través de texto en luagr
+     * de su dirección de memoria
+     */
+    @Test
+    public void shouldFindCopiesAOfBook(){
+        final Book bookToBeFound = new Book("libro-que-debe-encontrase","un título 1", "un autor");
+
+        BookCollection books = new BookCollection(new Book[]{
+                new Book("libro-que-debe-encontrase","un título 1", "un autor"),
+                new Book("un-isbn-2","un titulo 2", "un autor 2"),
+                new Book("libro-que-debe-encontrase","un título 1", "un autor"),
+                new Book("un-isbn-3","un titulo 3", "un autor 3")
+        });
+
+        List<Book> foundBooks = books.findCopies(bookToBeFound);
+
+        Assertions.assertFalse(foundBooks.isEmpty());
+
+        foundBooks.forEach((Book book)->Assertions.assertEquals(book,bookToBeFound));
+
+    }
+
+    /**
+     * Es necesario Assertions.fail() si no se lanza la excepción
+     */
+    @Test
+    public void shouldGetExceptionWhenUsingFailWithANonExistintEntry(){
+        final String titleToLocate = "un titulo inexistente";
+        BookCollection books = new BookCollection(new Book[]{
+                new Book("un-isbn-1","un titulo 1", "un autor 1"),
+                new Book("un-isbn-2","un titulo 2", "un autor 2"),
+                new Book("un-isbn-3","un titulo 3", "un autor 3")
+        });
+        try{
+            books.findOrFail(titleToLocate);
+            Assertions.fail("Se esperaba una excepción");
+        }
+        catch (Exception e){
+            Assertions.assertInstanceOf(BookCollection.ExpectedToFindAtLeastABook.class,e);
+        }
+
+    }
+
+    /**
+     * Similar al anterior pero utilizando el método assertThrows, que evita utilizar el método fail
+     */
+
+    @Test
+    public void shouldGetExceptionWhenUsingFailWithANonExistintEntryV2(){
+        final String titleToLocate = "un titulo inexistente";
+        BookCollection books = new BookCollection(new Book[]{
+                new Book("un-isbn-1","un titulo 1", "un autor 1"),
+                new Book("un-isbn-2","un titulo 2", "un autor 2"),
+                new Book("un-isbn-3","un titulo 3", "un autor 3")
+        });
+
+        Assertions.assertThrows(BookCollection.ExpectedToFindAtLeastABook.class, ()->books.findOrFail(titleToLocate));
+
+
+    }
+
+
 
 }
